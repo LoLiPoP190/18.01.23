5451335ac62939c215d2aa02f44769825b6bf1c5
LoLiPoP190 <atakoskonbaev21@gmail.com>
2023-01-18 15:09:58 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let myName = "Atak";
+
+console.log(myName);
+console.log("My name's length is " + myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.141592;
+
+console.log(pi);
+console.log(pi.toFixed(3));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2; //mdn to see priority
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+
+let example5 = 2 ** 4;
+let example6 = 81 ** (1/2);
+let example7 = 5 % 2;
+
+let example8 = "Hello " + 'World' + `!`;
+
+let example9 = 4 + 10 + 12;
+let example10 = 1 + 5 * 3;
+let example11 = 15 * (1 - 1);
+
+let example12 = 5 + '3';
+let example13 = '5' + 3;
+let example14 = '5' * 3;
+let example15 = 'a' * 3;
+
+let example16 = -15;
+let example17 = +'15';
+
+console.log(example17);
